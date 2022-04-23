a16c5a9ac2b43c824ec7c42cb8d3e71d513c07bb
iravishankarpal <iravishankarpal@gmail.com>
2022-04-23 11:29:31 +0530

    other aded status

--- Advance/hello.js
+++
@@ -1,9 +0,0 @@
-console.log("hellow"); //hello in termianl
-var k = 34;
-let sentence =`hellow ${k}`;
-console.log(`hellow ${k}`);
-console.log(sentence);
-
-
-var name = prompt('enter you name');
-document.getElementById('span').innerHTML=`hello ${name}`
